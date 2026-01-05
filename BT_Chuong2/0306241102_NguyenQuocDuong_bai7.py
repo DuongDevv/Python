9d442421c6d9f03a851744eed4207dd584756cbd
Nguyễn Quốc Đương <dathuynh594@gmail.com>
2026-01-05 10:49:07 +0700

    Add files via upload

---
+++ BT_Chuong2/0306241102_NguyenQuocDuong_bai7.py
@@ -0,0 +1,7 @@
+k = int(input("Nhap vao 1 so nguyen: "))
+if(k>0):
+    print(f"So nguyen dung truoc {k} la: {k-1}")
+    print(f"So nguyen dung sau {k} la: {k+1}")
+else:
+    print(f"So nguyen dung truoc {k} la: {k+1}")
+    print(f"So nguyen dung sau {k} la: {k-1}")
