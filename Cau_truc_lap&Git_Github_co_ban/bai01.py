a365ba9dda03686bf9cd3496d2bad23520103bdf
Nguyễn Quốc Đương <dathuynh594@gmail.com>
2026-01-20 10:14:28 +0700

    Add files via upload

---
+++ Cau_truc_lap&Git_Github_co_ban/bai01.py
@@ -0,0 +1,6 @@
+#Su dung for Loop de tinh tong so 100 so nguyen dau tien
+tong = 0
+for i in 101:
+    tong+=i
+    i+=1
+print(tong)
---
+++ Cau_truc_lap&Git_Github_co_ban/bai06.py
@@ -0,0 +1,5 @@
+mk = "admin123"
+inputpass =""
+inputpass = input("nhap mat khau: ")
+while(inputpass!=mk):
+    inputpass = input("Mat khau sai. Vui long nhap lai mat khau: ")
